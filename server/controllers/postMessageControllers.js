11d6a3b8f251f3f3072dd587ed15755a9fe5eac9
ChamikaRohan <chamika707@gmail.com>
2024-05-28 18:48:17 +0530

    Created file-upload route with firebase and multer

--- server/controllers/postMessageControllers.js
+++ server/controllers/postMessageControllers.js
@@ -1,4 +1,10 @@
 import PMessage from "../model/postMessageModel.js"
+import { initializeApp } from "firebase/app"
+import { getStorage, ref, getDownloadURL, uploadBytesResumable } from "firebase/storage"
+import fconfig from "../firebase/firebaseConfig.js"
+
+initializeApp(fconfig.firebaseConfig);
+const storage = getStorage();
 
 export const createPostMessage = async(req, res)=>{
     try{
@@ -7,7 +13,7 @@ export const createPostMessage = async(req, res)=>{
         const savedPmessage = await pMessage.save();
         res.status(200).json({message: "Post Message created succesfully!"})
     }
-    catch(err)
+    catch(err) 
     {
         res.status(404).json({error: err.message});
     }
@@ -21,5 +27,39 @@ export const getAllPostMessages = async(req, res)=>{
     catch(err)
     {
         res.status(404).json({error: err.message});
+    }
+}
+
+export const uploadFile = async (req, res) => {
+    try {
+      const currentDateTime = new Date();
+      if (!req.file) {
+        return res.status(400).json({ error: "No file uploaded" });
+      }
+  
+      const storageRef = ref(storage, `images/${req.file.originalname}+${currentDateTime}`);
+      const metadata = {
+        contentType: req.file.mimetype,
+      };
+  
+      const uploadTask = uploadBytesResumable(storageRef, req.file.buffer, metadata);
+  
+      uploadTask.on(
+        'state_changed',
+        (snapshot) => {
+          // Handle upload progress
+        },
+        (error) => {
+          // Handle upload error
+          res.status(500).json({ error: error.message });
+        },
+        async () => {
+          // Upload completed successfully, get the download URL
+          const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
+          res.status(200).json({ message: "File uploaded successfully!", downloadURL });
+        }
+      );
+    } catch (err) {
+      res.status(500).json({ error: err.message });
     }
 }
--- server/routes/postMessageRoutes.js
+++ server/routes/postMessageRoutes.js
@@ -1,10 +1,16 @@
 import express from "express"
-import {createPostMessage, getAllPostMessages} from "../controllers/postMessageControllers.js"
+import { initializeApp } from "firebase/app";
+import {createPostMessage, getAllPostMessages, uploadFile} from "../controllers/postMessageControllers.js"
+import fconfig from "../firebase/firebaseConfig.js"
+import multer from "multer"
+
+initializeApp(fconfig.firebaseConfig);
+const uploadFileMulter = multer({ storage: multer.memoryStorage() })
 
 const route = express.Router();
 
 route.post("/create-pmessage", createPostMessage);
 route.get("/get-allpmessages", getAllPostMessages);
-
+route.post("/create-pfile",uploadFileMulter.single('file'), uploadFile);
 
 export default route;
