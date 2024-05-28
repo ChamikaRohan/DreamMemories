4df9f1eacc4d459f8f4e83cdf86c49f40bbddac2
ChamikaRohan <chamika707@gmail.com>
2024-05-28 18:56:49 +0530

    Removed firebase credentials to a env file for security

--- server/firebase/firebaseConfig.js
+++ server/firebase/firebaseConfig.js
@@ -1,16 +1,16 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import dotenv from 'dotenv';
+dotenv.config();
 
 export default {
   firebaseConfig: 
   {
-      apiKey: "AIzaSyBFxVyBmWo88tjUWQI8qW9HnkJTUWEMQso",
-      authDomain: "dreammemories-9a11f.firebaseapp.com",
-      projectId: "dreammemories-9a11f",
-      storageBucket: "dreammemories-9a11f.appspot.com",
-      messagingSenderId: "1005755462548",
-      appId: "1:1005755462548:web:dcb5b97053cc16228b1581",
-      measurementId: "G-HLN7TPPJ19"
+      apiKey: process.env.apiKey,
+      authDomain: process.env.authDomain,
+      projectId: process.env.projectId,
+      storageBucket: process.env.storageBucket,
+      messagingSenderId: process.env.messagingSenderId,
+      appId: process.env.appId,
+      measurementId: process.env.measurementId
   }
 };
 
