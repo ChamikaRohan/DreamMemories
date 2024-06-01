251f18255a2a215e99894b96fd8b8d7df3619ccf
ChamikaRohan <chamika707@gmail.com>
2024-06-01 15:48:29 +0530

    Fixed some home page bugs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import SignupPage from './pages/SignupPage.jsx'
 import SigninPage from './pages/SigninPage.jsx'
-import Home from './pages/Home.jsx'
+import Home from "./pages/Home.jsx"
 import WelcomePage from './pages/WelcomePage.jsx'
 
 export default function AuthApp() {
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -9,51 +9,52 @@ import { checkUser } from "../middlewares/CheckUser.js"
 import authBg from '../assets/page_bg.jpg';
 
 export default function Home() {
-  const [userStatus, setUserStatus] = useState(false);
-  const apiURL = import.meta.env.VITE_API_BASE_URL;
-  const navigate = useNavigate();
-  const handleLogin = () =>{
-    navigate("/signup")
+    const [userStatus, setUserStatus] = useState(false);
+    const apiURL = import.meta.env.VITE_API_BASE_URL;
+    const navigate = useNavigate();
+    const handleLogin = () =>{
+      navigate("/signup")
+    }
+  
+    useEffect(() => {
+      const fetchUserStatus = async () => {
+        const checkedUser = await checkUser();
+        setUserStatus(checkedUser);
+      };
+  
+      fetchUserStatus();
+  }, []);
+  
+    return (
+      <Container maxWidth={false} disableGutters sx={{backgroundImage: `url(${authBg})`, backgroundSize: "cover", backgroundPosition: "center"}}>
+      <Container maxWidth="lg">
+        <CssBaseline />
+        <AppBar position='static' color='inherit' style={{ border: "black solid", borderWidth: 1 , marginTop: "10px", borderRadius: '20px', marginBottom: '20px', padding: '5PX' }}>
+          <Container sx={{display: "flex", justifyContent: "space-between", alignItems: "center"}}>
+              <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '10px' }}>
+                <Typography sx={{marginLeft: '20px',fontFamily: 'Poppins Medium',fontSize: {xs: '1.5rem', sm: '2rem',md: '3rem'}}} variant='h4' color="text.primary" align="center" >MEMORIES</Typography>
+                <Box component="img" src={memories} alt="Placeholder" sx={{width: {xs: '32px', sm: '40px', md: '50px'},height: 'auto'}}/>
+              </div>
+              <div style={{marginRight: "20px"}}>
+                <AccountCircleIcon onClick={handleLogin} sx={{width: {xs: '32px', sm: '40px', md: '50px'},height: 'auto'}} />
+              </div>
+          </Container>
+        </AppBar>
+        <Grow in>
+          <Container>
+            <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
+              <Grid item xs={12} sm={8}>
+                <Posts user={userStatus} />
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <Form user={userStatus} />
+              </Grid>
+            </Grid>
+          </Container>
+        </Grow>
+      </Container>
+      <Container sx={{ marginBottom: "20px"}} />
+      </Container>
+    );
   }
-
-  useEffect(() => {
-    const fetchUserStatus = async () => {
-      const checkedUser = await checkUser();
-      setUserStatus(checkedUser);
-    };
-
-    fetchUserStatus();
-}, []);
-
-  return (
-    <Container maxWidth={false} disableGutters sx={{backgroundImage: `url(${authBg})`, backgroundSize: "cover", backgroundPosition: "center"}}>
-    <Container maxWidth="lg">
-      <CssBaseline />
-      <AppBar position='static' color='inherit' style={{ border: "black solid", borderWidth: 1 , marginTop: "10px", borderRadius: '20px', marginBottom: '20px', padding: '5PX' }}>
-        <Container sx={{display: "flex", justifyContent: "space-between", alignItems: "center"}}>
-            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '10px' }}>
-              <Typography sx={{marginLeft: '20px',fontFamily: 'Poppins Medium',fontSize: {xs: '1.5rem', sm: '2rem',md: '3rem'}}} variant='h4' color="text.primary" align="center" >MEMORIES</Typography>
-              <Box component="img" src={memories} alt="Placeholder" sx={{width: {xs: '32px', sm: '40px', md: '50px'},height: 'auto'}}/>
-            </div>
-            <div style={{marginRight: "20px"}}>
-              <AccountCircleIcon onClick={handleLogin} sx={{width: {xs: '32px', sm: '40px', md: '50px'},height: 'auto'}} />
-            </div>
-        </Container>
-      </AppBar>
-      <Grow in>
-        <Container>
-          <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
-            <Grid item xs={12} sm={8}>
-              <Posts user={userStatus} />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Form user={userStatus} />
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
-    </Container>
-    <Container sx={{ marginBottom: "20px"}} />
-    </Container>
-  );
-}
+  
